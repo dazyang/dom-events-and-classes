3986222372eaa6af5c2e48c25e2a7c10e705cccf
dazyang <dazyang.contact@gmail.com>
2017-08-12 14:50:52 +1200

    Add hide

--- game.js
+++ game.js
@@ -12,7 +12,8 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
-    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('click', makeBlue
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -28,6 +29,12 @@ function makeBlue(evt){
     updateCounts()
 }
 // CREATE FUNCTION hide HERE
+function hide(evt) {
+    evt.target.classList.add('invisible')
+    updateCounts()
+}
+
+
 
 function updateCounts () {
   var totals = {
